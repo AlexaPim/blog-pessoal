994a78ea9f84af9bc5a94d53b96fb8c128a5af62
AlexaPim <alexssandraprado@hotmail.com>
2021-09-24 19:28:52 -0300

    Iniciando o blog pessoal

---
+++ src/main/java/org/generation/demoblogPessoal/DemoblogPessoalApplication.java
@@ -0,0 +1,13 @@
+package org.generation.demoblogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoblogPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoblogPessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/demoblogPessoal/DemoblogPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.demoblogPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoblogPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
