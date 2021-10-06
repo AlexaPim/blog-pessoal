19e6db0c58d3bb0abd9fca75ef3a562d51c2ece7
AlexaPim <alexssandraprado@hotmail.com>
2021-10-06 20:13:59 -0300

    Introducao ao Spring Security no blog pessoal

--- src/main/java/org/generation/demoblogPessoal/controller/TemaController.java
+++ src/main/java/org/generation/demoblogPessoal/controller/TemaController.java
@@ -36,9 +36,9 @@ public class TemaController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/nome/{nome}")
-	public ResponseEntity<List<Tema>> getByName(@PathVariable String nome){
-		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(nome));
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Tema>> getByName(@PathVariable String descricao){
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
 	
 	@PostMapping
--- src/main/java/org/generation/demoblogPessoal/repository/TemaRepository.java
+++ src/main/java/org/generation/demoblogPessoal/repository/TemaRepository.java
@@ -1,11 +1,11 @@
 package org.generation.demoblogPessoal.repository;
 
-import java.util.List;
-
-import org.generation.demoblogPessoal.model.Tema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TemaRepository extends JpaRepository<Tema, Long> {
+	import java.util.List;
 	
-	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
-}
+	import org.generation.demoblogPessoal.model.Tema;
+	import org.springframework.data.jpa.repository.JpaRepository;
+	
+	public interface TemaRepository extends JpaRepository<Tema, Long> {
+		
+		public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	}
