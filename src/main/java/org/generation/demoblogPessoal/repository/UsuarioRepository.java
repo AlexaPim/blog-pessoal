c99d91fe34afd0c93e593d35d88ca607742d8e2a
AlexaPim <alexssandraprado@hotmail.com>
2021-10-08 18:20:30 -0300

    Usando JUnit

--- src/main/java/org/generation/demoblogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/demoblogPessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package org.generation.demoblogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.demoblogPessoal.model.Usuario;
@@ -8,5 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	public Optional<Usuario> findByUsuario(String usuario);
+
+	public Usuario findByNome(String string);
+
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String string);
 	
 }
